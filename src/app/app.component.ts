2e8c41cf656d5adfdc16917d48d78814371fceb7
Joseph Kelley <josephkelley91@gmail.com>
2024-12-27 10:00:07 -0500

    clean up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,5 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
-import { CreditCardFormatterComponent } from './components/credit-card-formatter/credit-card-formatter.component';
-import { StarRatingComponent } from './components/star-rating/star-rating.component';
-import { DragAndDropComponent } from './components/drag-and-drop/drag-and-drop.component';
-import { MoviesSearchComponent } from './components/movies-search/movies-search.component';
 import { CommonModule } from '@angular/common';
 
 @Component({
